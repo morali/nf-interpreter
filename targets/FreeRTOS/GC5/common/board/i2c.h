1842e886e1df4329303f63e56b798b784680dabb
Mateusz Klatecki <mateusz.klatecki@gc5.pl>
2019-08-22 13:49:40 +0200

    Added function to initialize I2c2 and function to make transfer via i2c

--- targets/FreeRTOS/GC5/common/board/i2c.h
+++ targets/FreeRTOS/GC5/common/board/i2c.h
@@ -43,6 +43,7 @@ typedef struct
 }i2c_t;
     
 extern i2c_t i2c3;
+extern i2c_t i2c2;
 
 
 
@@ -54,12 +55,26 @@ extern i2c_t i2c3;
 /************************************************************************************************************/
 
 void I2C3_InitPeripheral(void);
+void I2C2_InitPeripheral(void);
 
 void I2C_MasterStructureInit(lpi2c_master_transfer_t *pTransfer, uint16_t slaveAddress, uint32_t subaddress,
                              size_t subaddressSize, uint32_t flags);
 
 void I2C_SetBufferAndDirection(lpi2c_master_transfer_t *pTransfer, uint8_t *buffer, size_t dataSize, lpi2c_direction_t direction);
 
+/**
+ * @brief  Do transfer via I2c
+ * @note   
+ * @param  *pi2c: Pointer to i2c_t structure
+ * @param  dev_addr: Device Address
+ * @param  direction: Direction Read or Write
+ * @param  subaddress: 
+ * @param  subaddressSize: 
+ * @param  *buff: Pointer to data to write or buffer where to read data
+ * @param  size: number of bytes to write or read
+ * @retval transfer status
+ */
+status_t I2CTransfer(i2c_t *pi2c, uint8_t dev_addr, lpi2c_direction_t direction, uint32_t subaddress, uint8_t subaddressSize, uint8_t *buff, uint16_t size);
 
 
 
