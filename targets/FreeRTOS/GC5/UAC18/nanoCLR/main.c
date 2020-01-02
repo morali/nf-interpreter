d86853feb79bc1b2e0f3f296444c86382d649f7c
Jeremi Jasiński <jeremi.jasinski@gc5.pl>
2020-01-02 07:41:56 +0100

    Increase thread stack size of PanelThread (GCC 9.2 fix)

--- targets/FreeRTOS/GC5/UAC18/nanoCLR/main.c
+++ targets/FreeRTOS/GC5/UAC18/nanoCLR/main.c
@@ -71,7 +71,7 @@ int main(void)
     xTaskCreate(ReceiverThread, "ReceiverThread", 2048, NULL, configMAX_PRIORITIES - 14, NULL);
     xTaskCreate(vRtcThread, "RtcThread", configMINIMAL_STACK_SIZE + 32, NULL, configMAX_PRIORITIES - 13, NULL);
     xTaskCreate(vMacAddressThread, "MacAddressThread", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 13, NULL);
-    xTaskCreate(vPanelThread, "PanelThread", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 14, NULL);
+    xTaskCreate(vPanelThread, "PanelThread", configMINIMAL_STACK_SIZE + 100, NULL, configMAX_PRIORITIES - 14, NULL);
     xTaskCreate(vLocalIOThread, "LocalIOhread", configMINIMAL_STACK_SIZE + 100, NULL, configMAX_PRIORITIES - 14, NULL);
     xTaskCreate(vBACnetThread, "bacnet", configMINIMAL_STACK_SIZE + 2048, NULL, (configMAX_PRIORITIES  - 14), NULL);
     
